3e56e88ea46f9fa48e70409d184a414cec7d6a78
Deven Kalra <ideven85@gmail.com>
2022-10-11 05:38:41 +0530

    Seed
    	new file:   .gitignore
    	new file:   .mvn/wrapper/maven-wrapper.jar
    	new file:   .mvn/wrapper/maven-wrapper.properties
    	new file:   mvnw
    	new file:   mvnw.cmd
    	new file:   pom.xml
    	new file:   src/main/java/com/cleo/CleoSocietyAPI/CleoSocietyApiApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/com/cleo/CleoSocietyAPI/CleoSocietyApiApplicationTests.java

---
+++ src/main/java/com/cleo/CleoSocietyAPI/CleoSocietyApiApplication.java
@@ -0,0 +1,13 @@
+package com.cleo.CleoSocietyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleoSocietyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleoSocietyApiApplication.class, args);
+	}
+
+}
